ae9e31884d8ca3fd81abf05f8d651efb6c5f3878
Balsubrahmanyam P <pbs9948@gmail.com>
2019-12-13 22:54:47 +0530

    udemy course added

--- src/01_variables.go
+++ src/01_variables.go
@@ -7,8 +7,16 @@ func main() {
 	var height, weight int = 30, 40
 	var name = "ironman"
 	address := "plot 301, highway, mars"
+	var negativeNumber int = -459
+	var sum int = -negativeNumber
+	var negativeFloating = -459.12
+	isDone := true
 	fmt.Println("hello world")
-	fmt.Println("My name is :", name, ".My age is ", age, ".My height is ", height, ".My weight is ", weight)
+	fmt.Println("My name is :", name)
+	fmt.Println("My age is ", age)
+	fmt.Println("My height is ", height)
+	fmt.Println("My weight is ", weight)
 	fmt.Println("My address is :", address)
-
+	fmt.Println(negativeNumber, sum, negativeFloating)
+	fmt.Println("My workdone? : ", isDone)
 }
