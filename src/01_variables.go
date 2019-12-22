4fe330668725bc26fa408dab1c9844c41e0b4dd7
jitendra <jithendrapitchuka@gmail.com>
2019-12-22 14:51:47 +0530

    variable types practice

--- src/01_variables.go
+++ src/01_variables.go
@@ -3,20 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	var age int = 30
-	var height, weight int = 30, 40
-	var name = "ironman"
-	address := "plot 301, highway, mars"
-	var negativeNumber int = -459
-	var sum int = -negativeNumber
-	var negativeFloating = -459.12
-	isDone := true
 	fmt.Println("hello world")
+
+
+	//  string
+	var name string = "Ironman"
 	fmt.Println("My name is :", name)
-	fmt.Println("My age is ", age)
-	fmt.Println("My height is ", height)
-	fmt.Println("My weight is ", weight)
-	fmt.Println("My address is :", address)
-	fmt.Println(negativeNumber, sum, negativeFloating)
-	fmt.Println("My workdone? : ", isDone)
+	
+
+
+
+    // integer
+	var height, weight int = 5, 62
+	fmt.Println("My height is ", height, " feet.My weight is : ", weight, "kg")
+
+
+	// boolean
+	isDone := true
+	var willRain = false
+	var isWild bool = true
+	fmt.Println("Is workdone? : ", isDone)
+	fmt.Println("Will it rain tommorrow :", willRain)
+	fmt.Println("Is cat wild :", isWild)
+
+	//find types
+	fmt.Printf("%T %T %T %T\n", name, height, isDone, willRain)
+
 }
